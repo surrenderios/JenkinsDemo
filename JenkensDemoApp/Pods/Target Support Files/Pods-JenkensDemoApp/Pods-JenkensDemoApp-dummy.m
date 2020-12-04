b0f21d23387e2f06444930d9f2012b78b337c92b
Alex_Wu <surrenderios@gmail.com>
2020-12-04 16:13:31 +0800

    init pod

---
+++ JenkensDemoApp/Pods/Target Support Files/Pods-JenkensDemoApp/Pods-JenkensDemoApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JenkensDemoApp : NSObject
+@end
+@implementation PodsDummy_Pods_JenkensDemoApp
+@end
---
+++ JenkensDemoApp/Pods/Target Support Files/Pods-JenkinsDemoApp_branch/Pods-JenkinsDemoApp_branch-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JenkinsDemoApp_branch : NSObject
+@end
+@implementation PodsDummy_Pods_JenkinsDemoApp_branch
+@end
